545f82ef89ff8622fc9b6e45724bf59a9b4a8ac6
Dewey <hammersdrichard@gmail.com>
2021-08-13 07:19:47 -0700

    Done.

---
+++ src/Bomb.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  count = () => {
+    if(this.state.secondsLeft === 0) {
+      return 'Boom!'
+    } else {
+      return `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+  }
+
+  render() {
+   return <div>{this.count()}</div>;
+  }
+}
